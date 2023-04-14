fd935558cc259b7ecb8fecdabdf5cf1ae784a20b
ansmeer008 <ansmeer008@gmail.com>
2023-04-14 22:30:42 +0900

    feat: create modal

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
+  <div class="backdrop" v-if="isOpen">
+    <div class="modal-box">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button v-on:click="isOpen = false">닫을거임</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
   </div>
 
   <div>
     <img src="./assets/room0.jpg" class="room-img" />
-    <h4>{{ products[0] }}</h4>
+    <h4 v-on:click="isOpen = true">{{ products[0] }}</h4>
     <p>50 만원</p>
     <button v-on:click="increase(0)">허위매물신고</button>
     <span>신고 수: {{ reportCount[0] }}</span>
@@ -34,6 +42,7 @@ export default {
       reportCount: [0, 0, 0],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
       menus: ["Home", "Products", "About"],
+      isOpen: false,
     };
   },
   components: {},
@@ -54,6 +63,14 @@ export default {
   color: #2c3e50;
 }
 
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
 .menu {
   background: darkslateblue;
   padding: 15px;
@@ -66,5 +83,18 @@ export default {
 .room-img {
   width: 100%;
   margin-top: 40px;
+}
+.backdrop {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.modal-box {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
