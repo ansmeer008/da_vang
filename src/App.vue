b4839d406e3a1e7678a850d304f707ff549d6610
ansmeer008 <ansmeer008@gmail.com>
2023-04-14 22:19:42 +0900

    feat: add list of rooms

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
+  </div>
+
+  <div>
+    <img src="./assets/room0.jpg" class="room-img" />
+    <h4>{{ products[0] }}</h4>
+    <p>50 만원</p>
+    <button v-on:click="increase(0)">허위매물신고</button>
+    <span>신고 수: {{ reportCount[0] }}</span>
+  </div>
+  <div>
+    <img src="./assets/room1.jpg" class="room-img" />
+    <h4>{{ products[1] }}</h4>
+    <p>가격은 아무거나</p>
+    <button v-on:click="increase(1)">허위매물신고</button>
+    <span>신고 수: {{ reportCount[1] }}</span>
+  </div>
+  <div>
+    <img src="./assets/room2.jpg" class="room-img" />
+    <h4>{{ products[2] }}</h4>
+    <p>가격은 아무거나</p>
+    <button v-on:click="increase(2)">허위매물신고</button>
+    <span>신고 수: {{ reportCount[2] }}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      reportCount: [0, 0, 0],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      menus: ["Home", "Products", "About"],
+    };
+  },
+  components: {},
+  methods: {
+    increase(i) {
+      this.reportCount[i]++;
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,6 +52,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
